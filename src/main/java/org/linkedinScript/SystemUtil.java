93bcc30a0f99305fce94396c1665671bc8ee1353
gadlo323 <gadlo323@gmail.com>
2022-06-10 18:12:08 +0300

    feat: start project with some config files

---
+++ src/main/java/org/linkedinScript/SystemUtil.java
@@ -0,0 +1,9 @@
+package org.linkedinScript;
+
+import java.lang.reflect.Field;
+import java.util.Map;
+
+public class SystemUtil {
+    public static final String LINKEDIN_USER = "https://www.linkedin.com/home";
+    public static final String LINKEDIN_PASSWORD = "gadlo323@gmail.com";
+}
